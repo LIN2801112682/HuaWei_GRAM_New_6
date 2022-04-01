523da3df5e9ea95356432aa087fcca84b4122c2e
LIN2801112682 <2801112682@qq.com>
2022-04-01 16:16:28 +0800

    查询优化

---
+++ src/matchQuery2/sortKey.go
@@ -0,0 +1,29 @@
+package matchQuery2
+
+type SortKey struct {
+	sizeOfInvertedList int
+	gram               string
+}
+
+func (s *SortKey) Pos() int {
+	return s.sizeOfInvertedList
+}
+
+func (s *SortKey) SetPos(pos int) {
+	s.sizeOfInvertedList = pos
+}
+
+func (s *SortKey) Gram() string {
+	return s.gram
+}
+
+func (s *SortKey) SetGram(gram string) {
+	s.gram = gram
+}
+
+func NewSortKey(pos int, gram string) SortKey {
+	return SortKey{
+		sizeOfInvertedList: pos,
+		gram:               gram,
+	}
+}
